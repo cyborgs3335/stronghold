76377c079f108d5caf65497ba8dd6dc126c16f7a
Brian Macy <macybk@gmail.com>
2016-03-18 23:19:39 -0500

    Fix bug in saving preferences?

--- DriveTrain2016/src/org/usfirst/frc/team3335/robot/RobotPreferences.java
+++ DriveTrain2016/src/org/usfirst/frc/team3335/robot/RobotPreferences.java
@@ -47,32 +47,32 @@ public class RobotPreferences {
     prefs = Preferences.getInstance();
 
     joystickScalar = prefs.getDouble(JOYSTICK_SCALAR, -1); // -0.9 old value
-    if (prefs.containsKey(JOYSTICK_SCALAR)) {
+    if (!prefs.containsKey(JOYSTICK_SCALAR)) {
       prefs.putDouble(JOYSTICK_SCALAR, joystickScalar);
     }
 
     joystickPowerScalar = prefs.getDouble(JOYSTICK_POWER_SCALAR, 2);
-    if (prefs.containsKey(JOYSTICK_POWER_SCALAR)) {
+    if (!prefs.containsKey(JOYSTICK_POWER_SCALAR)) {
       prefs.putDouble(JOYSTICK_POWER_SCALAR, joystickPowerScalar);
     }
 
     accelLimit = prefs.getDouble(ACCEL_LIMIT, 0.5);
-    if (prefs.containsKey(ACCEL_LIMIT)) {
+    if (!prefs.containsKey(ACCEL_LIMIT)) {
       prefs.putDouble(ACCEL_LIMIT, accelLimit);
     }
 
     joystickDriverPort = prefs.getInt(JOYSTICK_DRIVER_PORT, 0);
-    if (prefs.containsKey(JOYSTICK_DRIVER_PORT)) {
+    if (!prefs.containsKey(JOYSTICK_DRIVER_PORT)) {
       prefs.putInt(JOYSTICK_DRIVER_PORT, joystickDriverPort);
     }
 
     joystickShooterPort = prefs.getInt(JOYSTICK_SHOOTER_PORT, 2);
-    if (prefs.containsKey(JOYSTICK_SHOOTER_PORT)) {
+    if (!prefs.containsKey(JOYSTICK_SHOOTER_PORT)) {
       prefs.putInt(JOYSTICK_SHOOTER_PORT, joystickShooterPort);
     }
 
     drivetimer = prefs.getDouble(DRIVE_TIMER, 5);
-    if (prefs.containsKey(DRIVE_TIMER)) {
+    if (!prefs.containsKey(DRIVE_TIMER)) {
       prefs.putDouble(DRIVE_TIMER, drivetimer);
     }
 
